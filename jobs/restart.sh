ee45e7b999b11ac909074c4796f6c28c918dda0b
EricBoittier <eric.boittier@icloud.com>
2024-09-25 18:10:16 +0200

    restart slurm examples

--- jobs/restart.sh
+++ jobs/restart.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=test
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=20000
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=5000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=gpu02
+##SBATCH --nodelist=gpu02
 
 hostname
 
@@ -18,6 +18,9 @@ conda activate jaxe3xcuda11p39
 
 which python
 echo "GPU ID:" $CUDA_VISIBLE_DEVICES
-restart="/pchem-data/meuwly/boittier/home/jaxeq/all_runs/runs10/20240923-193354dcm-2-espw-10000.0-restart-False/best_10000.0_params.pkl"
-python /pchem-data/meuwly/boittier/home/jaxeq/dcmnet/main.py --type "dipole" --random_seed $RANDOM --n_dcm 2 --n_gpu $CUDA_VISIBLE_DEVICES --restart $restart
+
+echo "restart" $restart
+
+#restart="/pchem-data/meuwly/boittier/home/jaxeq/all_runs/runs11/"
+python /pchem-data/meuwly/boittier/home/jaxeq/dcmnet/main.py --type "dipole" --random_seed $RANDOM --n_gpu $CUDA_VISIBLE_DEVICES --restart $restart
 
