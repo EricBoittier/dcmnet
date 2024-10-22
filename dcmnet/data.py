8288937048e940062873e5ca324b18d22c875ee7
EricBoittier <eric.boittier@icloud.com>
2024-10-22 21:04:55 +0200

    notebooks on other tensor learning examples

--- dcmnet/data.py
+++ dcmnet/data.py
@@ -1,10 +1,11 @@
+import ase.data
 import e3x
 import jax
 import jax.numpy as jnp
 import numpy as np
-import ase.data
-from scipy.spatial.distance import cdist
 import pandas as pd
+from scipy.spatial.distance import cdist
+
 
 def cut_vdw(grid, xyz, elements, vdw_scale=1.4):
     """ """
@@ -19,7 +20,18 @@ def cut_vdw(grid, xyz, elements, vdw_scale=1.4):
     mask = ~mask.any(axis=1)
     return mask, closest_atom_type, closest_atom
 
-def prepare_multiple_datasets(key, num_train, num_valid, filename=["esp2000.npz"], clean=False):
+
+def prepare_multiple_datasets(
+    key,
+    num_train,
+    num_valid,
+    filename=["esp2000.npz"],
+    clean=False,
+    verbose=False,
+    esp_mask=False,
+    clip_esp=False,
+    natoms=60,
+):
     """
     Prepare multiple datasets for training and validation.
 
@@ -34,71 +46,186 @@ def prepare_multiple_datasets(key, num_train, num_valid, filename=["esp2000.npz"
     """
     # Load the datasets
     datasets = [np.load(f) for f in filename]
+    # datasets_keys() = datasets.keys()
+    if verbose:
+        for dataset in datasets:
+            for k, v in dataset.items():
+                print(k, v.shape)
 
-    for dataset in datasets:
-        for k, v in dataset.items():
-            print(k, v.shape)
+    if "id" in datasets[0].keys():
+        dataid = np.concatenate([dataset["id"] for dataset in datasets])
+    not_failed = None  # np.array(range().reshape(-1,6,3).shape[0]))
+    data = []
+    keys = []
 
-    dataid = np.concatenate([dataset["id"] for dataset in datasets])
     if clean:
-        failed = pd.read_csv("/pchem-data/meuwly/boittier/home/jaxeq/data/qm9-fails.csv")
+        failed = pd.read_csv(
+            "/pchem-data/meuwly/boittier/home/jaxeq/data/qm9-fails.csv"
+        )
         failed = list(failed["0"])
-        not_failed = [i for i in range(len(dataid)) if str(dataid[i]) not in failed ]
-        n_failed =  len(dataid) - len(not_failed)
+        not_failed = [i for i in range(len(dataid)) if str(dataid[i]) not in failed]
+        n_failed = len(dataid) - len(not_failed)
         print("n_failed:", n_failed)
         num_train = int(max([0, num_train - n_failed]))
         if num_train == 0:
-            num_valid = int(max([0, num_valid - n_failed]) )
+            num_valid = int(max([0, num_valid - n_failed]))
         print(num_train, num_valid)
-        
-    dataid = dataid[not_failed]
-    dataR = np.concatenate([dataset["R"] for dataset in datasets])[not_failed]
-    dataZ = np.concatenate([dataset["Z"] for dataset in datasets])[not_failed]
-    dataN = np.concatenate([dataset["N"] for dataset in datasets])[not_failed]
-    dataMono = np.concatenate([dataset["mono"] for dataset in datasets])[not_failed]
-    dataEsp = np.concatenate([dataset["esp"] for dataset in datasets])[not_failed]
-    dataVDW = np.concatenate([dataset["vdw_surface"] for dataset in datasets])[not_failed]
-    dataNgrid = np.concatenate([dataset["n_grid"] for dataset in datasets])[not_failed]
-    dataD = np.concatenate([dataset["D"] for dataset in datasets])[not_failed]
-    dataDxyz = np.concatenate([dataset["Dxyz"] for dataset in datasets])[not_failed]
-    dataCOM = np.concatenate([dataset["com"] for dataset in datasets])[not_failed]
-    print("creating_mask")
-    dataESPmask = np.array([cut_vdw(dataVDW[i], dataR[i], dataZ[i])[0] for i in range(len(dataZ))])
 
-    data = [
-        dataR,
-        dataZ,
-        dataN,
-        dataMono,
-        dataEsp,
-        dataVDW,
-        dataNgrid,
-        dataD,
-        dataDxyz,
-        dataCOM,
-        dataESPmask,
-        dataid,
-    ]
-    keys = [
-        "R",
-        "Z",
-        "N",
-        "mono",
-        "esp",
-        "vdw_surface",
-        "n_grid",
-        "D",
-        "Dxyz",
-        "com",
-        "espMask",
-        "id",
-    ]
-    assert_dataset_size(dataR, num_train, num_valid)
-    
+    shape = (
+        np.concatenate([dataset["R"] for dataset in datasets])
+        .reshape(-1, natoms, 3)
+        .shape
+    )
+    print("shape", shape)
+
+    if "id" in datasets[0].keys():
+        dataid = dataid[not_failed]
+        data.append(dataid)
+        keys.append("id")
+    if "R" in datasets[0].keys():
+        dataR = np.concatenate([dataset["R"] for dataset in datasets]).reshape(
+            -1, natoms, 3
+        )[not_failed]
+        data.append(dataR.squeeze())
+        keys.append("R")
+    if "Z" in datasets[0].keys():
+        dataZ = np.concatenate([dataset["Z"] for dataset in datasets]).reshape(
+            -1, natoms
+        )[not_failed]
+        data.append(dataZ.squeeze())
+        keys.append("Z")
+    if "F" in datasets[0].keys():
+        dataF = np.concatenate([dataset["F"] for dataset in datasets]).reshape(
+            -1, natoms, 3
+        )[not_failed]
+        data.append(dataF.squeeze())
+        keys.append("F")
+    if "E" in datasets[0].keys():
+        dataE = np.concatenate([dataset["E"] for dataset in datasets])[not_failed]
+        data.append(dataE.reshape(-1, 1))
+        keys.append("E")
+    if "N" in datasets[0].keys():
+        dataN = np.concatenate([dataset["N"] for dataset in datasets])[not_failed]
+        data.append(dataN.reshape(-1, 1))
+        keys.append("N")
+    if "mono" in datasets[0].keys():
+        dataMono = np.concatenate([dataset["mono"] for dataset in datasets])[not_failed]
+        data.append(dataMono.reshape(-1, natoms))
+        keys.append("mono")
+    if "esp" in datasets[0].keys():
+        if clip_esp:
+            dataEsp = np.concatenate([dataset["esp"][:1000] for dataset in datasets])[
+                not_failed
+            ].reshape(shape[0], 1000)
+        else:
+            dataEsp = np.concatenate([dataset["esp"] for dataset in datasets])[
+                not_failed
+            ]
+        data.append(dataEsp)
+        keys.append("esp")
+    if "vdw_surface" in datasets[0].keys():
+        dataVDW = np.concatenate([dataset["vdw_surface"] for dataset in datasets])[
+            not_failed
+        ]
+        data.append(dataVDW)
+        keys.append("vdw_surface")
+    if "n_grid" in datasets[0].keys():
+        dataNgrid = np.concatenate([dataset["n_grid"] for dataset in datasets])[
+            not_failed
+        ]
+        data.append(dataNgrid)
+        keys.append("n_grid")
+    if "D" in datasets[0].keys():
+        dataD = np.concatenate([dataset["D"] for dataset in datasets])[not_failed]
+        print("D", dataD.shape)
+        try:
+            data.append(dataD.reshape(-1, 1))
+        except:
+            data.append(dataD.reshape(shape[0], natoms, 3))
+        keys.append("D")
+    if "dipole" in datasets[0].keys():
+        dipole = np.concatenate([dataset["dipole"] for dataset in datasets]).reshape(
+            -1, 3
+        )
+        # [not_failed]
+        print("dipole.shape", dipole.shape)
+        data.append(dipole)
+        keys.append("dipole")
+    if "Dxyz" in datasets[0].keys():
+        dataDxyz = np.concatenate([dataset["Dxyz"] for dataset in datasets])[not_failed]
+        data.append(dataDxyz)
+        keys.append("Dxyz")
+    if "com" in datasets[0].keys():
+        dataCOM = np.concatenate([dataset["com"] for dataset in datasets])[not_failed]
+        data.append(dataCOM)
+        keys.append("com")
+    if "polar" in datasets[0].keys():
+        polar = np.concatenate([dataset["polar"] for dataset in datasets]).reshape(
+            -1, 3, 3
+        )
+        print("polar", polar.shape)
+        polar = polar  # [not_failed,:,:]
+        # print(polar)
+        data.append(polar)
+        keys.append("polar")
+    if "esp_grid" in datasets[0].keys():
+        esp_grid = np.concatenate(
+            [dataset["esp_grid"][:1000] for dataset in datasets]
+        ).reshape(-1, 1000, 3)
+        esp_grid = esp_grid  # [not_failed,:,:]
+        # print(polar)
+        data.append(esp_grid)
+        keys.append("esp_grid")
+    if "quadrupole" in datasets[0].keys():
+        quadrupole = np.concatenate(
+            [dataset["quadrupole"] for dataset in datasets]
+        ).reshape(-1, 3, 3)
+        quadrupole = quadrupole  # [not_failed,:,:]
+        # print(polar)
+        data.append(quadrupole)
+        keys.append("quadrupole")
+
+    for k in datasets[0].keys():
+        if k not in keys:
+            print(
+                k,
+                len(datasets[0][k].shape),
+                datasets[0][k].shape,
+                datasets[0][k].shape[0],
+            )
+            _ = np.concatenate([dataset[k] for dataset in datasets])
+            # [not_failed]
+            # if
+            print(k, _.shape)
+            data.append(_)
+            keys.append(k)
+
+    if esp_mask:
+        if verbose:
+            print("creating_mask")
+        dataESPmask = np.array(
+            [cut_vdw(dataVDW[i], dataR[i], dataZ[i])[0] for i in range(len(dataZ))]
+        )
+        data.append(dataESPmask)
+        keys.append("espMask")
+
+    print("R", dataR.shape)
+
+    assert_dataset_size(dataR.squeeze(), num_train, num_valid)
+
     return data, keys, num_train, num_valid
 
 
-def prepare_datasets(key, num_train, num_valid, filename, clean=False):
+def prepare_datasets(
+    key,
+    num_train,
+    num_valid,
+    filename,
+    clean=False,
+    esp_mask=False,
+    clip_esp=False,
+    natoms=60,
+):
     """
     Prepare datasets for training and validation.
 
@@ -115,9 +242,23 @@ def prepare_datasets(key, num_train, num_valid, filename, clean=False):
     if isinstance(filename, str):
         filename = [filename]
 
-    data, keys, num_train, num_valid = prepare_multiple_datasets(key, num_train, num_valid, filename, clean=clean)
-
-    train_choice, valid_choice = get_choices(key, len(data[0]), int(num_train), int(num_valid))
+    data, keys, num_train, num_valid = prepare_multiple_datasets(
+        key,
+        num_train,
+        num_valid,
+        filename,
+        clean=clean,
+        natoms=natoms,
+        clip_esp=clip_esp,
+        esp_mask=esp_mask,
+        # dataset_keys
+    )
+    print(data[0].shape)
+    print(keys)
+    print(len(data[0]))
+    train_choice, valid_choice = get_choices(
+        key, len(data[0]), int(num_train), int(num_valid)
+    )
 
     train_data, valid_data = make_dicts(data, keys, train_choice, valid_choice)
 
@@ -140,7 +281,6 @@ def assert_dataset_size(dataR, num_train, num_valid):
     assert num_valid >= 0
     # Make sure that the dataset contains enough entries.
     num_data = len(dataR)
-    print(num_data)
     num_draw = num_train + num_valid
     if num_draw > num_data:
         raise RuntimeError(
@@ -187,6 +327,7 @@ def make_dicts(data, keys, train_choice, valid_choice):
     train_data, valid_data = dict(), dict()
 
     for i, k in enumerate(keys):
+        print(i, k, len(data[i]), data[i].shape)
         train_data[k] = data[i][train_choice]
         valid_data[k] = data[i][valid_choice]
 
@@ -215,7 +356,11 @@ def print_shapes(train_data, valid_data):
     return train_data, valid_data
 
 
-def prepare_batches(key, data, batch_size, include_id=False, data_keys=None) -> list:
+def prepare_batches(
+    key, data, batch_size, 
+    include_id=False, data_keys=None, num_atoms=60,
+    dst_idx=None, src_idx=None
+) -> list:
     """
     Prepare batches for training.
 
@@ -230,7 +375,8 @@ def prepare_batches(key, data, batch_size, include_id=False, data_keys=None) -> 
         list: A list of dictionaries, each representing a batch.
     """
     # Determine the number of training steps per epoch.
-    data_size = len(data["mono"])
+
+    data_size = len(data["R"])
     steps_per_epoch = data_size // batch_size
 
     # Draw random permutations for fetching batches from the train data.
@@ -241,34 +387,43 @@ def prepare_batches(key, data, batch_size, include_id=False, data_keys=None) -> 
     perms = perms.reshape((steps_per_epoch, batch_size))
 
     # Prepare entries that are identical for each batch.
-    num_atoms = 60
+
     batch_segments = jnp.repeat(jnp.arange(batch_size), num_atoms)
     offsets = jnp.arange(batch_size) * num_atoms
-    dst_idx, src_idx = e3x.ops.sparse_pairwise_indices(num_atoms)
-    dst_idx = (dst_idx + offsets[:, None]).reshape(-1)
-    src_idx = (src_idx + offsets[:, None]).reshape(-1)
+    if dst_idx is None and src_idx is None:
+        print("sparse_pairwise_indices")
+        dst_idx, src_idx = e3x.ops.sparse_pairwise_indices(num_atoms)
+        dst_idx = (dst_idx + offsets[:, None]).reshape(-1)
+        src_idx = (src_idx + offsets[:, None]).reshape(-1)
+    elif dst_idx is not None and src_idx is not None:
+        dst_idx = data["dst_idx"]
+        src_idx = data["src_idx"]
+
+    # print(len(dst_idx))
 
     output = []
-    data_keys = [
-        "R",
-        "Z",
-        "N",
-        "mono",
-        "esp",
-        "vdw_surface",
-        "n_grid",
-        "D",
-        "Dxyz",
-        "espMask",
-        "com",
-    ]
-    if include_id:
-        data_keys.append("id")
+    if data_keys is None:
+        data_keys = [
+            "R",
+            "Z",
+            "N",
+            "mono",
+            "esp",
+            "vdw_surface",
+            "n_grid",
+            "D",
+            "Dxyz",
+            "espMask",
+            "com",
+        ]
+    # if include_id:
+    #     data_keys.append("id")
 
     for perm in perms:
         # print(perm)
         dict_ = dict()
         for k, v in data.items():
+            # print(k, v)
             if k in data_keys:
                 if k == "R":
                     dict_[k] = v[perm].reshape(-1, 3)
@@ -276,12 +431,18 @@ def prepare_batches(key, data, batch_size, include_id=False, data_keys=None) -> 
                     dict_[k] = v[perm].reshape(-1)
                 elif k == "mono":
                     dict_[k] = v[perm].reshape(-1)
-
                 else:
                     dict_[k] = v[perm]
 
-        dict_["dst_idx"] = dst_idx
-        dict_["src_idx"] = src_idx
+        if len(dst_idx.shape) > 1:
+            dict_["dst_idx"] = dst_idx[perm[0]]
+            # print(dict_["dst_idx"])
+        else:
+            dict_["dst_idx"] = dst_idx
+        if len(dst_idx.shape) > 1:
+            dict_["src_idx"] = src_idx[perm[0]]
+        else:
+            dict_["src_idx"] = src_idx
         dict_["batch_segments"] = batch_segments
         output.append(dict_)
 
